e26f4d64176ccf1f4d52239965041c753cc73810
joaofunes <joao.hfunes@gmail.com>
2015-06-03 23:21:33 -0600

    pantalla1

--- src/main/java/com/dimesa/managedbean/IndiceDeEncarrilamientoManagedBean.java
+++ src/main/java/com/dimesa/managedbean/IndiceDeEncarrilamientoManagedBean.java
@@ -5,7 +5,13 @@
  */
 package com.dimesa.managedbean;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -13,9 +19,93 @@ import org.springframework.web.context.WebApplicationContext;
  *
  * @author HDEZ
  */
-
 @Named("indiceDeEncarrilamientoManagedBean")
 @Scope(WebApplicationContext.SCOPE_SESSION)
 public class IndiceDeEncarrilamientoManagedBean {
+
+    private Date date1;
+    private Date date2;
+    private Date date3;
+    private boolean value1;
+    private boolean value2;
+    private boolean value3;
+    private boolean value4;
+
+
+    
+    public void onDateSelect(SelectEvent event) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+    }
+
+    public void click() {
+        RequestContext requestContext = RequestContext.getCurrentInstance();
+        requestContext.update("form:display");
+        requestContext.execute("PF('dlg').show()");
+        
+        System.out.println("hora "+getDate3());
+        System.out.println("hora "+getDate1());
+        System.out.println("hora "+isValue4());
+        System.out.println("hora "+isValue2());
+    }
+
+  
+    public Date getDate1() {
+        return date1;
+    }
+
+    public void setDate1(Date date1) {
+        this.date1 = date1;
+    }
+
+    public Date getDate2() {
+        return date2;
+    }
+
+    public void setDate2(Date date2) {
+        this.date2 = date2;
+    }
+
+    public boolean isValue1() {
+        return value1;
+    }
+
+    public void setValue1(boolean value1) {
+        this.value1 = value1;
+    }
+
+    public boolean isValue2() {
+        return value2;
+    }
+
+    public void setValue2(boolean value2) {
+        this.value2 = value2;
+    }
+
+    public boolean isValue3() {
+        return value3;
+    }
+
+    public void setValue3(boolean value3) {
+        this.value3 = value3;
+    }
+
+    public boolean isValue4() {
+        return value4;
+    }
+
+    public void setValue4(boolean value4) {
+        this.value4 = value4;
+    }
+
+    public Date getDate3() {
+        return date3;
+    }
+
+    public void setDate3(Date date3) {
+        this.date3 = date3;
+    }
+
     
 }
