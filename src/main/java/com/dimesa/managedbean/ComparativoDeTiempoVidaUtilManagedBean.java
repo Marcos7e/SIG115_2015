d12599f888daba34d3693c7a714446e639763b16
Joao Funes <joao.hfunes@gmail.com>
2015-06-10 09:26:55 -0600

    reportes

--- src/main/java/com/dimesa/managedbean/ComparativoDeTiempoVidaUtilManagedBean.java
+++ src/main/java/com/dimesa/managedbean/ComparativoDeTiempoVidaUtilManagedBean.java
@@ -5,17 +5,28 @@
  */
 package com.dimesa.managedbean;
 
+import com.dimesa.jasper.Reporte;
 import com.dimesa.managedbean.generic.GenericManagedBean;
 import com.dimesa.managedbean.lazymodel.IndicePromedioDeGastoReparacionEquipoLazyModel;
 import com.dimesa.model.Equipo;
+import com.dimesa.pojo.rpt.RptComparativoDeGastosReparacion;
 import com.dimesa.service.EquipoService;
 import com.dimesa.service.generic.GenericService;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.view.JasperViewer;
+import org.primefaces.context.RequestContext;
 import org.primefaces.model.LazyDataModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -39,9 +50,10 @@ public class ComparativoDeTiempoVidaUtilManagedBean extends GenericManagedBean<E
     private Date date3 = new Date();
     SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
     private String fecha;
-    private Date date1;
-    private Date date2;
+    private String equipox;
+    private String equipoy;
     private boolean value1;
+    private String reportName;
 
     @PostConstruct
     public void init() {
@@ -57,6 +69,43 @@ public class ComparativoDeTiempoVidaUtilManagedBean extends GenericManagedBean<E
     @Override
     public LazyDataModel<Equipo> getNewLazyModel() {
         return new IndicePromedioDeGastoReparacionEquipoLazyModel(equipoService);
+    }
+
+    public void click() {
+
+        if (getEquipox().equals(null) || getEquipox().equals("")) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Seleccione Equipo 1."));
+        } else if (getEquipoy().equals(null) || getEquipoy().equals("")) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Seleccione Equipo 2."));
+        } else if (getEquipox().toString().equals(getEquipoy().toString())) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Equipo 1 No Debe ser Igual a Equipo 2."));
+        } else {
+            print();
+        }
+
+    }
+
+    public void print() {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        List<RptComparativoDeGastosReparacion> list = new ArrayList<RptComparativoDeGastosReparacion>();
+        for (int i = 0; i < 100; i++) {
+            RptComparativoDeGastosReparacion prueba = new RptComparativoDeGastosReparacion();
+            prueba.setEquipox(12.2);
+            prueba.setEquipoy(Double.NaN);
+            list.add(prueba);
+        }
+
+        HttpServletRequest request = (HttpServletRequest) context.getRequest();
+        HttpServletResponse response = (HttpServletResponse) context.getResponse();
+        Reporte reporte = new Reporte("compgastosrep", "rpt_comparativo_gasto_reparaciones", request);
+
+        reporte.setDataSource(new JRBeanCollectionDataSource(new HashSet<RptComparativoDeGastosReparacion>(list)));
+        reporte.setReportInSession(request, response);
+        reportName = reporte.getNombreLogico();
+
+        JasperViewer.viewReport(reporte.getJasperPrint());/*quitar si funciona*/
+
+        RequestContext.getCurrentInstance().addCallbackParam("reportName", reportName);
     }
 
     public Date getDate3() {
@@ -91,22 +140,6 @@ public class ComparativoDeTiempoVidaUtilManagedBean extends GenericManagedBean<E
         this.equipo = equipo;
     }
 
-    public Date getDate1() {
-        return date1;
-    }
-
-    public void setDate1(Date date1) {
-        this.date1 = date1;
-    }
-
-    public Date getDate2() {
-        return date2;
-    }
-
-    public void setDate2(Date date2) {
-        this.date2 = date2;
-    }
-
     public boolean isValue1() {
         return value1;
     }
@@ -123,5 +156,30 @@ public class ComparativoDeTiempoVidaUtilManagedBean extends GenericManagedBean<E
     public void setFecha(String fecha) {
         this.fecha = fecha;
     }
+
+    public String getEquipox() {
+        return equipox;
+    }
+
+    public void setEquipox(String equipox) {
+        this.equipox = equipox;
+    }
+
+    public String getEquipoy() {
+        return equipoy;
+    }
+
+    public void setEquipoy(String equipoy) {
+        this.equipoy = equipoy;
+    }
+
+    public String getReportName() {
+        return reportName;
+    }
+
+    public void setReportName(String reportName) {
+        this.reportName = reportName;
+    }
+    
 
 }
